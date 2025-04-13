bf2866850bc5f0f74aa3fcbe93ee8ac048b9c1c2
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-04-13 18:28:38 -0300

    :sparkles: feat: Criando Rota De Cadastro De Usuario

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,13 @@
 import {Router, Request, Response} from 'express';
+import { UserController } from '../controllers/user/userController';
 
 const router = Router();
 
 router.get('/', (request:Request, responde: Response) => {
     responde.send("Hello Wolrd");
 });
+
+router.post('/user', new UserController().handle);
 
 
 export {router};
