5a95a22a161812dc70091758edae390ff3190f7f
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-04-13 18:23:09 -0300

    :recycle: refactor: Rescrevendo Para Variaveis De Ambiente

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,10 +1,15 @@
 import {Client} from 'pg';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const postgresClient = new Client({
-    user: 'postgres',
-    password: 'admin',
-    host: 'localhost',
-    port: 5432
+    user: process.env.USER_POSTGRES_NAME,
+    password: process.env.USER_POSTGRES_PASSWORD,
+    host: process.env.USER_POSTGRES_HOST,
+    port: Number(process.env.USER_POSTGRES_PORT),
+    database: process.env.USER_POSTGRES_DATABASE
 });
 
 export default postgresClient;
