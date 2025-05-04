6fad738334be731001ece6ba761775ec73d6080d
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-05-04 19:19:01 -0300

    :sparkles: feat: Criando controller de filmes

---
+++ src/multer/multer.ts
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import {resolve} from 'path'
+
+export default {
+    upload(folder:string){
+        return {
+            storage:multer.diskStorage({
+                destination:resolve(__dirname,'..','..',folder),
+                filename(req,file,cb){
+
+
+                    const fileName = file.originalname
+                    return cb(null, fileName);
+                }
+            }),
+        }
+    }
+}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,8 +2,15 @@ import {Request, Response} from 'express';
 import express from 'express';
 import { UserController } from '../controllers/user/userController';
 import { AuthUserController } from '../controllers/auth/authUserController';
+import { MovieController } from '../controllers/movie/movieController';
+import multerConfig from '../multer/multer';
+
+import multer from 'multer'
 
 const router = express.Router();
+
+const upload = multer(multerConfig.upload('uploads'));
+
 
 router.get('/' , (request:Request, response:Response ) => {
     response.send("Hello World");
@@ -12,5 +19,7 @@ router.get('/' , (request:Request, response:Response ) => {
 router.post('/user', new UserController().create);
 
 router.post('/auth', new AuthUserController().authenticate);
+
+router.post('/movie',upload.single('banner'), new MovieController().createMovie);
 
 export {router};
